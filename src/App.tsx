30c9a1f54aba73551a24204d93b3c62c0e7c8e4c
Linus Gellhorn <linus.g@hotmail.com>
2021-11-21 21:14:33 +0000

    Added frontend behaviour for training, feeding, ignoring and rehoming

--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,22 @@ function App() {
             name: "Walk",
             handler: () => loadDataFromEndpoint("/digipet/walk"),
           },
-          { name: "Feed" },
+          {
+            name: "Feed",
+            handler: () => loadDataFromEndpoint("/digipet/feed"),
+          },
+          {
+            name: "Train",
+            handler: () => loadDataFromEndpoint("/digipet/train"),
+          },
+          {
+            name: "Ignore",
+            handler: () => loadDataFromEndpoint("/digipet/ignore"),
+          },
+          {
+            name: "Rehome!",
+            handler: () => loadDataFromEndpoint("/digipet/rehome"),
+          },
         ]}
       />
     </main>
